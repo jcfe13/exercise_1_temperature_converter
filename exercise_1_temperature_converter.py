49d8c5f515faf98c9719647e698182045c217d4d
jcfe13 <johncarloesquillo72@gmail.com>
2024-09-24 21:13:57 +0800

    New line

--- exercise_1_temperature_converter.py
+++ exercise_1_temperature_converter.py
@@ -5,8 +5,10 @@ temperature = float(input("Input the temperature: "))
 print("\nConversions:")
 print("1. Celsius to Fahrenheit")
 print("2. Fahrenheit to Celsius")
+
 # 3. Ask the user to select the conversion types
 conversion_type = input("\nSelect the number for conversion type: ")
+
 # 4. Create the conditions
 if conversion_type == "1":
     result = (temperature * 9 / 5) + 32  # formula to convert °C to °F
@@ -15,4 +17,4 @@ elif conversion_type == "2":
     result = (temperature - 32) * 5 / 9  # formula to convert °F to °C
     print(f"\nThe conversion of {temperature}°F to Celsius is: {round(result, 2)}°C")
 else:
-    print("\n\033[91mInvalid character/number!\033[0m") 
+    print("\n\033[91mInvalid character/number!\033[0m") # show error message for invalid input
