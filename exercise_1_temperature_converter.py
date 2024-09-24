621f3ade441c9d82a491c3f917c006fb086d0341
jcfe13 <johncarloesquillo72@gmail.com>
2024-09-24 20:56:37 +0800

    Ask the user to input the temperature

--- exercise_1_temperature_converter.py
+++ exercise_1_temperature_converter.py
@@ -1,4 +1,5 @@
 # 1. Ask the user to input the temperature
+temperature = float(input("Input the temperature: "))
 # 2. Display the conversion types
 # 3. Ask the user to select the conversion types
 # 4. Create the conditions
