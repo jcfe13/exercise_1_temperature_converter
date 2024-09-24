bb7f89ee140e02f2ba44e71542e53af7ccde950f
jcfe13 <johncarloesquillo72@gmail.com>
2024-09-24 20:58:30 +0800

    Display the conversion types

--- exercise_1_temperature_converter.py
+++ exercise_1_temperature_converter.py
@@ -2,5 +2,8 @@
 temperature = float(input("Input the temperature: "))
 
 # 2. Display the conversion types
+print("\nConversions:")
+print("1. Celsius to Fahrenheit")
+print("2. Fahrenheit to Celsius")
 # 3. Ask the user to select the conversion types
 # 4. Create the conditions
