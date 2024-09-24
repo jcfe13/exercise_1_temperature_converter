6562f213f251b04bf9bb6a3524628576261cf476
jcfe13 <johncarloesquillo72@gmail.com>
2024-09-24 21:04:43 +0800

    Retry

--- exercise_1_temperature_converter.py
+++ exercise_1_temperature_converter.py
@@ -7,6 +7,3 @@ print("1. Celsius to Fahrenheit")
 print("2. Fahrenheit to Celsius")
 # 3. Ask the user to select the conversion types
 conversion_type = input("\nSelect the number for conversion type: ")
-# 4. Create the conditions
-if conversion_type == "1":
-    result = (temperature * 9 / 5) + 32 # formula to convert °C to °F
